7dadebf59f3f1854ee194e75ac68daeef7c8e742
2805733637@qq.com <2805733637@qq.com>
2026-02-02 20:22:15 +0800

    Initial commit

---
+++ src/components/PersonalInformation.vue
@@ -0,0 +1,27 @@
+﻿<script setup lang="ts">
+import { UserFilled } from '@element-plus/icons-vue'
+</script>
+
+<template>
+  <div class="user">
+    <el-avatar :icon="UserFilled" shape="square" class="avatar"/>
+    <div class="username">F1uffySn0w</div>
+  </div>
+</template>
+
+<style scoped>
+.user {
+  display: flex;
+  width: 150px;
+  margin-top: 12px;
+  margin-left: 7px;
+  margin-bottom: 12px;
+}
+
+.username {
+  margin-left: 10px;
+  margin-top: 6px;
+  font-size: 18px;
+  color: white;
+}
+</style>
---
+++ src/pages/Home.vue
@@ -0,0 +1,62 @@
+﻿<script setup lang="ts">
+import PersonalInformation from "../components/PersonalInformation.vue";
+</script>
+
+<template>
+  <el-container>
+    <el-aside>
+      <div class="header-context">
+        <!-- 个人信息显示 -->
+        <el-aside class="personal-information">
+          <PersonalInformation />
+        </el-aside>
+
+        <!-- 分隔符 也不知道有啥用 暂时先放这 -->
+        <el-divider class="divider" direction="vertical" content-position="center" />
+      </div>
+    </el-aside>
+  </el-container>
+</template>
+
+<style>
+
+.el-header {
+  padding-left: 0;
+  padding-right: 0;
+  width: auto;
+  height: auto;
+}
+
+.el-container {
+  margin: 0;
+}
+
+body {
+  background-color: #141414;
+}
+
+body {
+  margin: 0;
+}
+
+.nav-tabs > .el-tabs__content {
+  text-align: center;
+  color: white;
+}
+
+.header-context {
+  display: flex;
+  width: 180px;
+  border-bottom: 1px white solid;
+  border-right: 1px white solid;
+}
+
+.divider {
+  margin-top: 25px;
+}
+
+.personal-information {
+  width: auto;
+  height: auto;
+}
+</style>
